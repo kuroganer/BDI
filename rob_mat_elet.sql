a97c0152f79133fb5d015776d6cf0b237caf5f87
kuroganer <tobikyoshiro@gmail.com>
2025-03-31 11:56:11 -0300

    Add files via upload

---
+++ rob_mat_elet.sql
@@ -0,0 +1,10 @@
+create database mat_elet;
+create table Funcionario
+(
+idFuncionario int not null auto_increment
+Nome varchar(60) not null
+CPF varchar(30) not null
+salario varchar(20) not null
+primary key (idFuncionario)
+);
+
