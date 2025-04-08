93122675835f4b18f4ee6a78bc376076030d159e
kuroganer <tobikyoshiro@gmail.com>
2025-04-08 09:47:33 -0300

    Add files via upload

--- BDi/rob_mat_elet.sql
+++ BDi/rob_mat_elet.sql
@@ -1,77 +1,28 @@
-/* create database mat_elet
-create table departamentos
-(
-idDepartamentos int(11) NOT NULL AUTO_INCREMENT,
-  Nome varchar(60) NOT NULL,
- localizacao varchar(30) NOT NULL,
-  PRIMARY KEY (idDepartamentos)
-
-);
-
-
-create table funcionarios
-(
-idFuncionario int not null auto_increment,
-Nome varchar(60) not null,
-CPF char(15) not null,
-salario varchar(20) not null,
-idDepartamentos_fk int(11)not null,
-primary key (idFuncionario),
-foreign key(idDepartamentos_fk) references departamentos(idDepartamentos)
-
-);
-
-CREATE TABLE dependentes (
-  idDependentes int(11) NOT NULL AUTO_INCREMENT,
-  Nome varchar(60) NOT NULL,
-  grau_parentesco varchar(30) NOT NULL,
-  idFuncionario_fk int(11) not null,
-  PRIMARY KEY (idDependentes),
-  foreign key(idFuncionario_fk) references funcionarios(idFuncionario)
-);
-
-
- CREATE TABLE projetos (
-  idProjetos int(11) NOT NULL AUTO_INCREMENT,
-  Nome varchar(60) NOT NULL,
-  Verba varchar(30) NOT NULL,
-  Tipo varchar(30) NOT NULL,
-  Numero char(30) NOT NULL,
-  idFuncionario_fk int(11) not null,
-  idDepartamentos_fk int(11)not null,
-  PRIMARY KEY (idProjetos),
-  foreign key(idFuncionario_fk) references funcionarios(idFuncionario),
-  foreign key(idDepartamentos_fk) references departamentos(idDepartamentos)
-) ;
-
-
-CREATE TABLE telefone (
-  idTelefone int(11) NOT NULL AUTO_INCREMENT,
-  DDI char(4) NOT NULL,
-  DDD varchar(3) NOT NULL,
-  Operadora varchar(30) NOT NULL,
-  Numero varchar(15) NOT NULL,
-   idFuncionario_fk int(11) not null,
-   idDependentes_fk int(11) NOT NULL,
-  PRIMARY KEY (idTelefone),
-  foreign key(idFuncionario_fk) references funcionarios(idFuncionario),
-  foreign key(idDependentes_fk) references dependentes(idDependentes)
-) 
-
-CREATE TABLE endereco (
-  idEndereco int(11) NOT NULL AUTO_INCREMENT,
-  bairro varchar(60) NOT NULL,
-  complemento varchar(60) NOT NULL,
-  logradouro varchar(30) NOT NULL,
-  Numero varchar(30) NOT NULL,
-  cidade varchar(30) NOT NULL,
-  UF varchar(30) NOT NULL,
-  Pais varchar(30) NOT NULL,
-  CEP varchar(30) NOT NULL,
-   idFuncionario_fk int(11) not null,
-   idDependentes_fk int(11) NOT NULL,
-PRIMARY KEY (idEndereco),
-  foreign key(idFuncionario_fk) references funcionarios(idFuncionario),
-  foreign key(idDependentes_fk) references dependentes(idDependentes)
-);
-;*/
+/*insert into departamentos(nome, localizacao)
+values("Gerência","SDF01"),("RH","SDF02"),("Presidência", "SDF10"),("Recepção","SDF01"),
+("Almoxarifado","SDF03");
+/*select * from departamentos;
+/*insert into funcionarios(nome, cpf, salario, idDepartamentos_fk)
+values("Pedro Ferreira", "005.612.333-71","R$ 10.000,00",1),("Ana Luiza Silva", "35.222.772-04","R$2.000,00",4),
+("Beatriz Pereira", "004.242.153-18","R$15.000,00",2),("José Leite", "127.772.124-25","R$70.000,00",3),
+("Raimundo Nonato","111.111.111-11", "R$ 2.000,00",5);
+/*select * from funcionarios;
+/*delete from funcionarios where idfuncionario = 2;
+/* insert into funcionarios(nome, cpf, salario, idDepartamentos_fk)
+values ("Ana Luiza Silva", "035.222.772-04","R$2.000,00",4);
+/*select * from funcionarios;
+/*insert into dependentes(nome, grau_parentesco, idfuncionario_fk)
+values ("Geraldo Magela", "Filho", 1), ("Michel Jackson", "Cônjuge",3),("Maria Cardoso", "Enteada", 6),
+("Roberto Justus", "Sogro", 4),("José Alcântara","Neto",5);
+/*SELECT * FROM DEPENDENTES, FUNCIONARIOS;
+/*INSERT INTO TELEFONE(ddi, ddd, operadora, numero, idfuncionario_fk,iddependentes_fk)
+values("+55", "061", "Vivo", "99475-7282",1,1);
+/*INSERT INTO TELEFONE(ddi, ddd, operadora, numero, idfuncionario_fk,iddependentes_fk)
+values("+55", "051", "Claro", "99998-5524",3,2),("+55","071", "Tim","99744-2286",6,3),
+("+55","077", "OI","98475-2244",4,4), ("+55", "024", "Vivo", "99934-4343",5,5);
+insert into endereco(bairro, complemento, logradouro, numero, cidade, uf, pais, cep, idfuncionario_fk,iddependentes_fk)
+values ("P-Norte","Conj F", "Qnp 5","27", "Ceilândia","DF","Brasil","72.224-800",1,1)
+/*insert into endereco(bairro, complemento, logradouro, numero, cidade, uf, pais, cep, idfuncionario_fk,iddependentes_fk)
+values ("Setor O","Conj b", "Qno 22","24", "Ceilândia","DF","Brasil","70.232-815",3,2),("Taguatinga sul","Conj z", "Qnl 24","33", "Taguatinga","DF","Brasil","71.204-720",6,3),
+ ("Eixo Norte","Quadra B apt", "310 Norte ","24", "Asa Norte","DF","Brasil","75.284-132",4,4), ("W3 Sul","Quadra b apartamento", "512 Sul","24", "Asa Sul","DF","Brasil","68.215-22",5,5)
+ */
